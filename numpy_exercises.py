b846eedda8726bb4a33c450f0bfea4684eb374a3
Corey Solitaire <corey.solitaire@gmail.com>
2020-08-10 14:22:53 -0500

    Corrected Formatting Problems

--- numpy_exercises.py
+++ numpy_exercises.py
@@ -16,12 +16,22 @@ a = a[a > 0]
 a = a[a % 2 == 0]
 print(len(a))
 
+#Even Numbers
+(a % 2 == 0)
+#Positive Numbers
+(a > 0)
+#Even and Positive
+a[(a % 2 == 0) & (a > 0)]
+#Count(shape) Even and Positive
+a[(a % 2 == 0) & (a > 0)].shape
+
 # 4. If you were to add 3 to each data point, how many positive numbers would there be?
 
 a = np.array([4, 10, 12, 23, -2, -1, 0, 0, 0, -6, 3, -7])
 a = a + 3
 a = a[a > 0]
 print(len(a))
+
 
 # 5. If you squared each number, what would the new mean and standard deviation be?
 
@@ -52,48 +62,89 @@ z_score
 import numpy as np
 
 ## Setup 1
-a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+# Life w/o numpy to life with numpy
+aa = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 # Use python's built in functionality/operators to determine the following:
 # Exercise 1 - Make a variable called sum_of_a to hold the sum of all the numbers in above list
 
-sum_of_a = a.sum()
-sum_of_a
+# sum_of_a = a.sum()
+# sum_of_a
+
+total = 0
+for n in aa:
+    total += n
+sum_of_a = total
+print(sum_of_a)
 
 # Exercise 2 - Make a variable named min_of_a to hold the minimum of all the numbers in the above list
 
-min_of_a = a.min()
-min_of_a
+# min_of_a = a.min()
+# min_of_a
+
+a.sort()
+min_of_a = a[0]
+print(min_of_a)
 
 # Exercise 3 - Make a variable named max_of_a to hold the max number of all the numbers in the above list
 
-max_of_a = a.max()
-max_of_a
+# max_of_a = a.max()
+# max_of_a
+
+max_of_a = max(a)
 
 # Exercise 4 - Make a variable named mean_of_a to hold the average of all the numbers in the above list
 
-mean_of_a = a.mean()
-mean_of_a
+# mean_of_a = a.mean()
+# mean_of_a
+
+mean_of_a = (sum_of_a /len(a))
+print(mean_of_a)
 
 # Exercise 5 - Make a variable named product_of_a to hold the product of multiplying all the numbers in the above list together
 
-product_of_a = a.prod()
-product_of_a
+# product_of_a = a.prod()
+# product_of_a
+
+total = 1
+for n in aa:
+    total *= n
+product_of_a = total
+print(product_of_a)
+
 
 # Exercise 6 - Make a variable named squares_of_a. It should hold each number in a squared like [1, 4, 9, 16, 25...]
 
-squares_of_a = np.square(a)
-squares_of_a
+# squares_of_a = np.square(a)
+# squares_of_a
+
+squares_of_a = [(n ** 2) for n in aa]
+print(squares_of_a)
 
 # Exercise 7 - Make a variable named odds_in_a. It should hold only the odd numbers
 
-odds_in_a = a[a % 2 != 0]
-odds_in_a
+# odds_in_a = a[a % 2 != 0]
+# odds_in_a
+
+oddlist = [] 
+for n in aa: 
+    if (n % 2 != 0): 
+         oddlist.append(n) 
+    odds_in_a = oddlist
+print(oddlist)
 
 # Exercise 8 - Make a variable named evens_in_a. It should hold only the evens.
 
-evens_in_a = a[a % 2 == 0]
-evens_in_a
+# evens_in_a = a[a % 2 == 0]
+# evens_in_a
+
+evenlist = [] 
+for n in aa: 
+    if (n % 2 == 0): 
+         evenlist.append(n) 
+    even_in_a = evenlist
+print(evenlist)
 
 ## What about life in two dimensions? A list of lists is matrix, a table, a spreadsheet, a chessboard...
 ## Setup 2: Consider what it would take to find the sum, min, max, average, sum, product, and list of squares for this list of two lists.
@@ -102,7 +153,8 @@ b = np.array([[3, 4, 5],
               [6, 7, 8]])
 b
 
-# Exercise 1 - refactor the following to use numpy. Use sum_of_b as the variable. **Hint, you'll first need to make sure that the "b" variable is a numpy array**
+# Exercise 1 - refactor the following to use numpy. Use sum_of_b as the variable. 
+#              **Hint, you'll first need to make sure that the "b" variable is a numpy array**
 
 sum_of_b = 0
 for row in b:
@@ -230,14 +282,17 @@ var_c
 print(np.shape(c))
 
 # Exercise 5 - Transpose c and print out transposed result.
-c = np.transpose(c)
-print(c)
+t_c = np.transpose(c)
+print(t_c)
 
 # Exercise 6 - Get the dot product of the array c with c. 
 
-
+dot_product = np.dot(c,t_c)
+dot_product
 
 # Exercise 7 - Write the code necessary to sum up the result of c times c transposed. Answer should be 261
+
+(c * t_c).sum()
 
 # Exercise 8 - Write the code necessary to determine the product of c times c transposed. Answer should be 131681894400.
 
